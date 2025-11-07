89fbda8f8bee8dd1f347c7ee01a6a0671a457845
Saswat Rath <saswat.iare25@gmail.com>
2025-11-08 02:37:19 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -7,6 +7,7 @@ public class Fibonacci {
         int n = sc.nextInt();
         int a = 0, b = 1;
         System.out.print(a + " " + b + " ");
+        
         for (int i = 3; i <= n; i++) {
             int c = a + b;
             System.out.print(c + " ");
