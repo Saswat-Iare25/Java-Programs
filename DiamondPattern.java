98401d49a7fc13a057f8f6d0707c5fa8f854c95f
Saswat Rath <saswat.iare25@gmail.com>
2025-11-08 02:37:05 +0530

    Update DiamondPattern.java

--- DiamondPattern.java
+++ DiamondPattern.java
@@ -8,6 +8,7 @@ public class DiamondPattern {
         for (int i = 1; i <= n; i++) {
             for (int j = i; j < n; j++) System.out.print(" ");
             for (int j = 1; j <= (2 * i - 1); j++) System.out.print("*");
+            
             System.out.println();
         }
         for (int i = n - 1; i >= 1; i--) {
