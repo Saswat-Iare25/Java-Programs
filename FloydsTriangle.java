ce7a22e960c054986a36f57c99b249120616f097
Saswat Rath <saswat.iare25@gmail.com>
2025-11-08 02:37:24 +0530

    Update FloydsTriangle.java

--- FloydsTriangle.java
+++ FloydsTriangle.java
@@ -8,6 +8,7 @@ public class FloydsTriangle {
         int num = 1;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
+                
                 System.out.print(num + " ");
                 num++;
             }
