241ff73ee78d18ece39cc085671c5f613f5f369d
Saswat Rath <saswat.iare25@gmail.com>
2025-11-10 19:23:14 +0530

    Update CozaLozaWoza.java

--- CozaLozaWoza.java
+++ CozaLozaWoza.java
@@ -1,3 +1,4 @@
+
 public class CozaLozaWoza {
     public static void main(String[] args) {
         for (int i = 1; i <= 110; i++) {
@@ -7,6 +8,7 @@ public class CozaLozaWoza {
             if (i % 3 != 0 && i % 5 != 0 && i % 7 != 0) System.out.print(i);
             System.out.print(" ");
             if (i % 11 == 0) System.out.println();
+            
         }
     }
 }
