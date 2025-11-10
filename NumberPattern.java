64f4994e69ac41484e661bb2068923c5b1e86218
Saswat Rath <saswat.iare25@gmail.com>
2025-11-10 19:24:39 +0530

    Update NumberPattern.java

--- NumberPattern.java
+++ NumberPattern.java
@@ -5,6 +5,7 @@ public class NumberPattern {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter height: ");
         int n = sc.nextInt();
+        
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) System.out.print(j + " ");
             System.out.println();
