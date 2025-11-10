c8d047d16a0c63b9914503192bb4455b4e0ea4c5
Saswat Rath <saswat.iare25@gmail.com>
2025-11-10 19:24:22 +0530

    Update IntegerRepresentation.java

--- IntegerRepresentation.java
+++ IntegerRepresentation.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class IntegerRepresentation {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        
         System.out.print("Enter an integer: ");
         int num = sc.nextInt();
         System.out.println("Decimal: " + num);
