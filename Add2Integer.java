97303e025b15e01029395777c6dacb606afef09f
Saswat Rath <saswat.iare25@gmail.com>
2025-11-10 19:06:56 +0530

    Update Add2Integer.java

--- Add2Integer.java
+++ Add2Integer.java
@@ -3,12 +3,15 @@ import java.util.Scanner;
 
 public class Add2Integer {
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter first integer: ");
         int a = sc.nextInt();
         System.out.print("Enter second integer: ");
         int b = sc.nextInt();
         System.out.println("Sum = " + (a + b));
+        
         sc.close();
+        
     }
 }
