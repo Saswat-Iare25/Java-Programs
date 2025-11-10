ee5662cd6a80ee37a3ead22900f2cacf93fdd2d1
Saswat Rath <saswat.iare25@gmail.com>
2025-11-10 19:07:43 +0530

    Update Armstrong.java

--- Armstrong.java
+++ Armstrong.java
@@ -5,11 +5,13 @@ public class Armstrong {
     public static void main(String[] a) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(), t = n, s = 0, d = (int) Math.log10(n) + 1;
+        
         while (t > 0) {
             int r = t % 10;
             s += Math.pow(r, d);
             t /= 10;
         }
         System.out.println(s == n ? "Armstrong" : "Not Armstrong");
+        
     }
 }
