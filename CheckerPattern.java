658de8bccb67d860f628aa0a98d6f1f79897fa9d
Saswat Rath <saswat.iare25@gmail.com>
2025-11-10 19:22:54 +0530

    Update CheckerPattern.java

--- CheckerPattern.java
+++ CheckerPattern.java
@@ -4,6 +4,7 @@ public class CheckerPattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter size: ");
+        
         int n = sc.nextInt();
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
