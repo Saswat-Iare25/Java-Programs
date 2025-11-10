23e9652451897794b2600abc61579e121cd851b2
Saswat Rath <saswat.iare25@gmail.com>
2025-11-10 19:22:26 +0530

    Update ButterflyPattern.java

--- ButterflyPattern.java
+++ ButterflyPattern.java
@@ -10,6 +10,7 @@ public class ButterflyPattern {
             for (int j = 1; j <= 2 * (n - i); j++) System.out.print(" ");
             for (int j = 1; j <= i; j++) System.out.print("*");
             System.out.println();
+            
         }
         for (int i = n; i >= 1; i--) {
             for (int j = 1; j <= i; j++) System.out.print("*");
