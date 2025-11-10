795cbbe800d05a41d57a94ac8b2d08b6176bc82b
Saswat Rath <saswat.iare25@gmail.com>
2025-11-10 19:23:27 +0530

    Update Disarium.java

--- Disarium.java
+++ Disarium.java
@@ -6,6 +6,7 @@ public class Disarium {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(), t = n, s = 0, d = (int) Math.log10(n) + 1;
         while (t > 0) {
+            
             int r = t % 10;
             s += Math.pow(r, d--);
             t /= 10;
