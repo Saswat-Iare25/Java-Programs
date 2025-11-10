60ede793e95261892fcb31d007812fb3eb0c6f56
Saswat Rath <saswat.iare25@gmail.com>
2025-11-10 19:22:22 +0530

    Update BoxPattern.java

--- BoxPattern.java
+++ BoxPattern.java
@@ -6,6 +6,7 @@ public class BoxPattern {
         System.out.print("Enter size: ");
         int n = sc.nextInt();
         for (int i = 1; i <= n; i++) {
+            
             for (int j = 1; j <= n; j++) {
                 if (i == 1 || i == n || j == 1 || j == n)
                     System.out.print("* ");
