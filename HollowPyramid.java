a0ae5d979937f4674d74b1ae034e7e5a29ad1be4
Saswat Rath <saswat.iare25@gmail.com>
2025-11-10 19:24:10 +0530

    Update HollowPyramid.java

--- HollowPyramid.java
+++ HollowPyramid.java
@@ -5,6 +5,7 @@ public class HollowPyramid {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter height: ");
         int n = sc.nextInt();
+        
         for (int i = 1; i <= n; i++) {
             for (int j = i; j < n; j++) System.out.print(" ");
             for (int j = 1; j <= (2 * i - 1); j++) {
