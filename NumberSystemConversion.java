79f7b8941ed676931701f86754cd42a45fb5ea72
Saswat Rath <saswat.iare25@gmail.com>
2025-11-10 19:24:45 +0530

    Update NumberSystemConversion.java

--- NumberSystemConversion.java
+++ NumberSystemConversion.java
@@ -5,6 +5,7 @@ public class NumberSystemConversion {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a decimal number: ");
         int n = sc.nextInt();
+        
         System.out.println("Binary: " + Integer.toBinaryString(n));
         System.out.println("Octal: " + Integer.toOctalString(n));
         System.out.println("Hexadecimal: " + Integer.toHexString(n).toUpperCase());
