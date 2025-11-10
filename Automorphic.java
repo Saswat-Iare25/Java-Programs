09ac47f9f6a70a1da6f7e79b1355f614cf384fd4
Saswat Rath <saswat.iare25@gmail.com>
2025-11-10 19:22:04 +0530

    Update Automorphic.java

--- Automorphic.java
+++ Automorphic.java
@@ -5,6 +5,7 @@ public class Automorphic {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         System.out.println((n * n + "").endsWith(n + "") ? "Automorphic" : "Not Automorphic");
+        
     }
     
 }
