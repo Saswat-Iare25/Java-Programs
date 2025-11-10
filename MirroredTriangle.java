b2af18e7a75f10f607d7d4e0fc813ce30adb3738
Saswat Rath <saswat.iare25@gmail.com>
2025-11-10 19:24:32 +0530

    Update MirroredTriangle.java

--- MirroredTriangle.java
+++ MirroredTriangle.java
@@ -5,6 +5,7 @@ public class MirroredTriangle {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter height: ");
         int n = sc.nextInt();
+        
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n - i; j++) System.out.print(" ");
             for (int j = 1; j <= i; j++) System.out.print("*");
