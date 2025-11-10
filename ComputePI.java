ca00fb4bec94c734b5c1a63fd4dbd2f2dc8bc824
Saswat Rath <saswat.iare25@gmail.com>
2025-11-10 19:23:07 +0530

    Update ComputePI.java

--- ComputePI.java
+++ ComputePI.java
@@ -7,6 +7,7 @@ public class ComputePI {
         int n = sc.nextInt();
         double pi = 0.0;
         for (int i = 0; i < n; i++) {
+            
             if (i % 2 == 0) pi += 1.0 / (2 * i + 1);
             else pi -= 1.0 / (2 * i + 1);
         }
